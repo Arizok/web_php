f1ea6cd738418a434be96b4901dbcd6fa164857a
Arizok <arizok82@gmail.com>
2024-10-28 17:12:32 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>KHAMLAR-HA</title>
+  <link rel="icon" href="./img/xing.png" type="image/png" sizes="16x16">
+  <link rel="stylesheet" href="./tools/bootstrap-icons.min.css">
+  <link rel="stylesheet" href="./tools/bootstrap.min.css">
+  <link rel="stylesheet" href="./tools/style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Lao:wght@500&display=swap" rel="stylesheet">
+</head>
+
+<body>
+  <!-- nav ------------------------------------------- -->
+  <nav class="navbar navbar-expand-lg nav__bg sticky-top">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"><img src="./img/xing.png" class="logo__nav" alt=""></a>
+      <button class="navbar-toggler btn__toggler" type="button" type="button" data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+        <i class="bi bi-list"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link hover__nav active__nav" href="#"><i class="bi bi-house"></i> ໜ້າຫຼັກ</a>
+
+          </li>
+          <li class="nav-item">
+            <a class="nav-link hover__nav" href="#"><i class="bi bi-feather"></i> ນົກທິສວຍທິສຸດ</a>
+          </li>
+        </ul>
+        <div class="d-flex">
+          <a href="#" class="hover__nav"><i class="bi bi-person"></i> ກຽ່ວກັບ</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+
+
+
+
+
+  <div class="offcanvas offcanvas-start offcanvas__bg" tabindex="-1" id="offcanvasExample"
+    aria-labelledby="offcanvasExampleLabel">
+    <div class="offcanvas-header offcanvas__header">
+      <h5 class="offcanvas-title" id="offcanvasExampleLabel"><img src="./img/xing.png" class="logo__sidebar" alt="">
+      </h5>
+      <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link hover__nav active__nav" href="#"><i class="bi bi-house"></i> ໜ້າຫຼັກ <span
+              class="text__active"><i class="bi bi-brightness-low"></i></span></a>
+
+        </li>
+        <li class="nav-item">
+          <a class="nav-link hover__nav" href="#"><i class="bi bi-feather"></i> ນົກທິສວຍທິສຸດ</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link hover__nav" href="#"><i class="bi bi-person"></i> ກຽ່ວກັບ</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+  <!-- nav ------------------------------------------- -->
+
+
+  <!-- img first ------------------------------------------- -->
+
+
+  <div class="container-fluid d-none d-lg-block">
+    <div class="row">
+      <div class="col-sm-6">
+        <p class="text__welcome">ຍິນດີຕ້ອນຮັບ</p>
+        <p class="textsub__welcome">ເຂົ້າສູ່ເວັບຂ້ອຍ</p>
+        <center><a href="#Readmore" class="Readmore">ອ່ານເພີ່ມ <i class="bi bi-node-plus-fill"></i></a></center>
+      </div>
+
+      <div class="col-sm-6">
+        <img src="./img/1516613555.png" class="img__first" alt="">
+      </div>
+    </div>
+  </div>
+
+
+
+
+  <div class="container-fluid d-lg-none">
+    <div class="row">
+      <div class="col-sm-6">
+        <img src="./img/1516613555.png" class="img__firsts" alt="">
+      </div>
+
+      <div class="col-sm-6">
+        <p class="text__welcomes">ຍິນດີຕ້ອນຮັບ</p>
+        <p class="textsub__welcomes">ເຂົ້າສູ່ເວັບຂ້ອຍ</p>
+        <center><a href="#Readmore" class="Readmores">ອ່ານເພີ່ມ <i class="bi bi-node-plus-fill"></i></a></center>
+      </div>
+
+    </div>
+  </div>
+
+  <p class="text__br"></p>
+
+  <div class="container" id="Readmore">
+
+
+    <div class="row mb-5">
+      <div class="col-sm-1 p-0">
+        <img src="img/web.png" class="w-100 h-100" alt="">
+      </div>
+      <div class="col-sm-11 p-0">
+        <h2><b>ກຽ່ວກັບ</b></h2>
+        <p class="text__p1">ສະບາຍດີທຸກທ່ານທີ່ເຂົ້າມາເບີ່ງຂໍ້ມູນໃນເວັບໄຊນີ້. ເວັບໄຊນີ້ຖືກສ້າງຂື້ນມາເພື່ອສຶກສາຫາຄວາມຮູ້
+          ເເລະ ຄວາມບັນເທີງເທົ່ານັ້ນ. ໃນເວັບໄຊນີ້ຈະພາເຮົາໄປຮູ້ຈັກຈຳນວນນົກທີ່ສວຍທີ່ສຸດໃນໂລກເລີຍກໍ່ວ່າໄດ້.
+          ຂໍ້ມູນຕ່າງໆທີ່ໄດ້ສະຂຽນລົງໃນເວັບນີ້ ຫວັງວ່າທ່ານຈະຮັບພິຈາລະນາ ເເລະ ສຶກສາໄປພ້ອມກັນ.</p>
+      </div>
+    </div>
+
+    <div class="row mt-5">
+      <div class="col-sm-1"></div>
+      <div class="col-sm-10">
+
+
+        <div class="row">
+          <div class="col-sm-3 col-6">
+            <center><a href="./img/49203313562_26428c5a7b_b.jpg"><img src="./img/49203313562_26428c5a7b_b.jpg"
+                  class="img__show_one" alt=""></a></center>
+          </div>
+          <div class="col-sm-3 col-6">
+            <center><a href="./img/11118661.jpg"><img src="./img/11118661.jpg" class="img__show_one" alt=""></a>
+            </center>
+          </div>
+
+          <div class="col-sm-3 col-6">
+            <center><a href="./img/6029887491.jpg"><img src="./img/6029887491.jpg" class="img__show_one" alt=""></a>
+            </center>
+          </div>
+
+          <div class="col-sm-3 col-6">
+            <center><a href="./img/IMG_32151.jpg"><img src="./img/IMG_32151.jpg" class="img__show_one" alt=""></a>
+            </center>
+          </div>
+        </div>
+
+
+
+        <div class="row">
+          <div class="col-sm-3 col-6">
+            <center><a href="./img/Quetzal-Wallpaper-77962.jpg"><img src="./img/Quetzal-Wallpaper-77962.jpg"
+                  class="img__show_one" alt=""></a></center>
+          </div>
+          <div class="col-sm-3 col-6">
+            <center><a href="./img/1200.jpg"><img src="./img/1200.jpg" class="img__show_one" alt=""></a></center>
+          </div>
+
+          <div class="col-sm-3 col-6">
+            <center><a href="./img/122002.jpg"><img src="./img/122002.jpg" class="img__show_one" alt=""></a></center>
+          </div>
+
+          <div class="col-sm-3 col-6">
+            <center><a href="./img/1200 (2).jpg"><img src="./img/1200 (2).jpg" class="img__show_one" alt=""></a>
+            </center>
+          </div>
+        </div>
+
+      </div>
+    </div>
+    
+    <div class="row">
+      <div class="col-sm-12 mt-5">
+        <h5 class="mt-5"><b>ເເຜນທີຂອງຜູ້ພັດທະນາ</b></h5>
+        <iframe
+      src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d37666.16514289551!2d102.57668217728457!3d18.0120577879667!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2sla!4v1728378595295!5m2!1sen!2sla"
+      width="100%" height="450" style="border:0; border-radius: 10px;" allowfullscreen="" class="mb-5" loading="lazy"
+      referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+
+    </div>
+
+
+  </div>
+
+
+
+
+
+
+</body>
+<footer class="my__footer">
+
+  <div class="row">
+    <div class="col-sm-4 d-none d-lg-block">
+      <center><img src="./img/line 01.png" class="w-75" alt=""></center>
+    </div>
+    <div class="col-sm-4 d-none d-lg-block">
+
+      <center>
+        <p class="footer__text">ຊ່ອງທາງຕິດຕໍ່</p>
+        <p class="face__book"><i class="bi bi-facebook"></i> ກ້າວໄປດ້ວຍກັນ</p>
+        <p class="WhatsApp"><i class="bi bi-whatsapp"></i> 20 76 456 308</p>
+      </center>
+
+    </div>
+
+    <div class="col-sm-4 d-lg-none">
+
+
+      <center>
+        <p class="footer__texts">ຊ່ອງທາງຕິດຕໍ່</p>
+        <p class="face__books"><i class="bi bi-facebook"></i> ກ້າວໄປດ້ວຍກັນ</p>
+        <p class="WhatsApps"><i class="bi bi-whatsapp"></i> 20 76 456 308</p>
+      </center>
+
+    </div>
+    <div class="col-sm-4 d-none d-lg-block">
+      <center><img src="./img/line 01.png" class="w-75" alt=""></center>
+    </div>
+  </div>
+
+
+  <script src="./tools/bootstrap.bundle.min.js"></script>
+  <script src="./tools/jquery-3.7.1.js"></script>
+  <script src="./tools/script.js"></script>
+</footer>
+
+
+
+</html>
